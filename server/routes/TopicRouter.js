18f4e603c57dbafd2b51d30c0030d0af8397cb3f
electrone901 <Carbajalluis@rocketmail.com>
2017-02-01 18:56:48 -0500

    close#27

--- server/routes/TopicRouter.js
+++ server/routes/TopicRouter.js
@@ -8,31 +8,31 @@ const createTopic = (req, res) => {
 		// StudentId: req.body.StudentId
 	})
 	.then((data) => {
-		res.send(data)
-	})
-}
+		res.send(data);
+	});
+};
 
 
 const getTopics = (req, res) => {
 	models.Topic.findAll()
 	.then((topics) => {
-		res.send(topics)
-	})
-}
+		res.send(topics);
+	});
+};
 
 
 const getOneTopic = (req, res) => {
 	models.Topic.findById(req.params.id)
 	.then((data) => {
-		res.send(data)
-	})
-}
+		res.send(data);
+	});
+};
 
 
 const deleteTopic = (req, res) => {
 	models.Topic.destroy({ where: { id: req.params.id } })
-	.then(()=>res.send('has been deleted'))
-}
+	.then(()=>res.send('has been deleted'));
+};
 
 
 const updateTopic = (req, res) => {
@@ -40,18 +40,18 @@ const updateTopic = (req, res) => {
 	models.Topic.findOne({ where: { id: req.params.id } })
 	.then((topicInfo)=>topicInfo.update({name: req.body.name})
    )
-	.then((data)=> res.send(data))
-}
+	.then((data)=> res.send(data));
+};
 
 
 //ROUTES
 router.route('/')
 	.post(createTopic)
-	.get(getTopics)
+	.get(getTopics);
 
 router.route('/:id')
 	.get(getOneTopic)
 	.delete(deleteTopic)
-	.put(updateTopic)
+	.put(updateTopic);
 
 module.exports = router;
